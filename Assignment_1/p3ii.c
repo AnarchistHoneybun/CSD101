a71091f32afa05054b7b00ecac695eac8ec2156e
Nirvana <thatonebipanda@gmail.com>
2022-10-23 08:18:57 +0530

    Changed naming convention

---
+++ Assignment_1/p3ii.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("      /\\    \n");
+    printf("     /  \\   \n");
+    printf("    /    \\  \n");
+    printf("   ________  \n");
+    return 0;
+}
---
+++ Assignment_1/p3iii.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf(" _______    \n");
+    printf("|       |   \n");
+    printf("|       |   \n");
+    printf(" _______    \n");
+    return 0;
+}
